391ccbad5d832f71f2561fd7d214f033409ebc42
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-11-15 05:37:53 -0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import WeatherApp from "./Components/WeatherApp/WeatherApp";
 function App() {
   return (
     <div className="App">
-      <WeatherApp />;
+      <WeatherApp />
     </div>
   );
 }
